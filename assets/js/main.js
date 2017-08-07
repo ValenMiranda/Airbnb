88f3e75ef18acd5649c6eba1a4dc3c082251d2de
Antonia Cerda <antoniacerdag@gmail.com>
2017-08-07 13:00:08 -0400

    impriendo json en pantalla

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,12 +7,21 @@ $(document).ready(function() {
 			var img = ele.listing.thumbnail_url;
 			var precio = ele.pricing_quote.rate.amount;
 			var usd = ele.pricing_quote.rate.currency;
+			var imgUser = ele.listing.user.picture_url;
+			var tipoPieza = ele.listing.room_type;
+			var cantHuesped = ele.listing.person_capacity;
+			var puntuacion = ele.listing.star_rating;
+			var descripcion = ele.listing.name;
 			console.log(img);
 			console.log(precio);
 			console.log(usd);
-			//$(".alojamiento").append("<div class='img-lugar'><img src='"+img+"'><div class='precio'><p class='precio'"+precio+"</p><div></div>");
+			console.log(imgUser);
+			console.log(tipoPieza);
+			console.log(cantHuesped);
+			console.log(puntuacion);
+			console.log(descripcion);
+			$(".contenedor").append("<li><div class='img-lugar'><img src='"+img+"'></div><div class='precio'><p class='precio'>"+precio+"</p><div><div class='imgUser'><img src='"+imgUser+"'><div class='descripcion'><p class='description-txt'>"+descripcion+"</p></li>");
 		})
-		//console.log(e.search_results);
 	})
 	
 });

5ef1d9385155584279dbbea408c60115e67b0f1e
cstapiarubio <cstapiarubio@gmail.com>
2017-08-08 13:27:43 -0400

    listo el marcado en google maps

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,12 +27,22 @@ google.maps.event.addListener(marker,'click', function(e){
 });*/
  var map;
       function initMap() {
+      	var myLatLng = {lat: 42.3601, lng: -71.0589};
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 42.3601, lng: -71.0589},
           zoom: 8
         });
 
-        google.maps.event.addListener(map, 'click',
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
+
+    }
+
+        /*google.maps.event.addListener(map, 'click',
         	function(event){
         		addMarker({coords:event.lasLng});
         	});
@@ -70,22 +80,11 @@ marker.addListener('click', function(){
 
       addMarker({
       	coords:{lat:42.4668, lng:-70.9495},
-      	iconImage:'https://developers.gogle.com/maps/documentation/javascript/examples/full/images/beachflag.png'});
+      	iconImage:'http://1.bp.blogspot.com/-PxgBgllxJrE/VpgleojkmyI/AAAAAAAAEAs/NOibbG2gPUc/s1600/google-marker-preview1-177x300.png'});
       
       addMarker({coords:{lat:42.8584, lng:-70.9300}});
       addMarker({coords:{lat:42.7762, lng:-71.0773}});
 
-    /*function addMarker(props) {
-    	var marker = new google.maps.Marker({
-    		position:props.coords,
-    		map:map,
-    		//icon:'https://developers.google'
-    	});
-    	if 
-    }*/
-
-      addMarker({coords:{lat: 42.8584, lng: -70.9300}});
-      addMarker({coords:{lat: 42.7762, lng: -71.0773}});
 
       function addMarker(props){
       	var marker = new google.maps.Marker({
@@ -102,4 +101,4 @@ marker.addListener('click', function(){
       			content:props.content
       		});
       	}
-      }
+      }*/
--- dist/js/script.js
+++ dist/js/script.js
@@ -20283,12 +20283,22 @@ google.maps.event.addListener(marker,'click', function(e){
 });*/
  var map;
       function initMap() {
+      	var myLatLng = {lat: 42.3601, lng: -71.0589};
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 42.3601, lng: -71.0589},
           zoom: 8
         });
 
-        google.maps.event.addListener(map, 'click',
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
+
+    }
+
+        /*google.maps.event.addListener(map, 'click',
         	function(event){
         		addMarker({coords:event.lasLng});
         	});
@@ -20326,22 +20336,11 @@ marker.addListener('click', function(){
 
       addMarker({
       	coords:{lat:42.4668, lng:-70.9495},
-      	iconImage:'https://developers.gogle.com/maps/documentation/javascript/examples/full/images/beachflag.png'});
+      	iconImage:'http://1.bp.blogspot.com/-PxgBgllxJrE/VpgleojkmyI/AAAAAAAAEAs/NOibbG2gPUc/s1600/google-marker-preview1-177x300.png'});
       
       addMarker({coords:{lat:42.8584, lng:-70.9300}});
       addMarker({coords:{lat:42.7762, lng:-71.0773}});
 
-    /*function addMarker(props) {
-    	var marker = new google.maps.Marker({
-    		position:props.coords,
-    		map:map,
-    		//icon:'https://developers.google'
-    	});
-    	if 
-    }*/
-
-      addMarker({coords:{lat: 42.8584, lng: -70.9300}});
-      addMarker({coords:{lat: 42.7762, lng: -71.0773}});
 
       function addMarker(props){
       	var marker = new google.maps.Marker({
@@ -20358,4 +20357,4 @@ marker.addListener('click', function(){
       			content:props.content
       		});
       	}
-      }
+      }*/

bfe0dc9626cef35dc60e3a1af4788538ab30213c
cstapiarubio <cstapiarubio@gmail.com>
2017-08-09 11:30:41 -0400

    añadiendo ubicacion con precios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,104 +1,85 @@
-/*var mapOptions = {
-	center: new google.maps.LatLng(37.7831, -122.4039),
-	zoom:12,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-};
+/*$(document).ready(function(){
+    res.forEach(function(el){
+    console.log(el);
+    var baseDatos = el.search_results;
+    console.log(baseDatos);
+    baseDatos.forEach(function(e){
+  var precios=e.pricing_quote.rate.amount;
+  console.log(precios);
+ if (precios <= example_id.input.val()){
+$("#mapa").append('<span id="precios">'+ precios + '</span>');
+  }
+  
+  })
+  })
 
-
-var map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
-var acOptions = {
-	types: 
-}
-
-var markerOptions = {
-	position: new google.maps.LatLng(37.7831, -122.4039)
-};
-
-var marker = new google.maps.Marker(markerOptions);
-marker.setMap(map);
-
-var infoWindowOptions = {
-	content:'hola'
-};
-
-var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
-google.maps.event.addListener(marker,'click', function(e){
-	infoWindow.open(map, marker);
-});*/
- var map;
+   var map;
       function initMap() {
-      	var myLatLng = {lat: 42.3601, lng: -71.0589};
+        var myLatLng = {lat: 42.3601, lng: -71.0589};
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 42.3601, lng: -71.0589},
           zoom: 8
         });
 
+var inputDestino=(document.getElementById('autocompletado'));//conectado input cami filtros
+var autocompleteDestino = new google.maps.places.Autocomplete(inputDestino);
+autocompleteDestino.bindTo('bounds', map);
 
         var marker = new google.maps.Marker({
-          position: myLatLng,
+          position: inputDestino,
           map: map,
-          title: 'Hello World!'
-        });
+          draggable: true,
+    icon: image,
+    label: {
+      text: precios,
+      color: 'black',
+      fontSize: '15px',
+      fontWeight: 'bold'
+        }
+})
+      }
 
-    }
-
-        /*google.maps.event.addListener(map, 'click',
-        	function(event){
-        		addMarker({coords:event.lasLng});
-        	});
-
-        var marker = new google.maps.Marker({
-      	position: { lat: 42.4668, lng: -70.9495},
-      	map:map,
-      	icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
       });
-var infoWindow = new google.maps.InfoWindow({
-	content:'<p>hola</p>'
-});
+ 
+      google.maps.event.addDomListener(window, "load", initMap); */
+      $(document).ready(function(){
+        res.forEach(function(el){
+          console.log(el);
+          var baseDatos = el.search_results;
+          console.log(baseDatos);
+          baseDatos.forEach(function(e){
+            var precios=e.pricing_quote.rate.amount;
+            console.log(precios);
 
-marker.addListener('click', function(){
-	infoWindow.open(map, marker);
-});
+            function initialize() {
+              var map = new google.maps.Map(
+                document.getElementById('map'), {
+                  center: new google.maps.LatLng(41.385932, 2.178678),
+                  zoom: 13,
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+                });
+              var image = {
+                url: 'http://www.homedepot.com/catalog/swatchImages/35/04/04a604de-8b52-4cd8-a394-6286f00b438d_35.jpg',
+          // This marker is 35 pixels wide by 35 pixels high.
+          size: new google.maps.Size(35, 35),
+          // The origin for this image is (0, 0).
+          origin: new google.maps.Point(0, 0),
+          // The anchor for this image is the base of the flagpole at (0, 32).
+          anchor: new google.maps.Point(0, 35)
+        };
+        var clickMarker = new google.maps.Marker({
+          position: map.getCenter(),
+          map: map,
+          draggable: true,
+          icon: image,
+          label: {
+            text: precios,
+            color: 'black',
+            fontSize: '15px',
+            fontWeight: 'bold'
+          }
+        });
       }
-
-
-      var markers = [
-      {
-      	coords:{lat:42.4668, lng:-709495},
-      	iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-      	content:'<p>hola</p>'
-      },
-
-      {
-      	coords:{lat:42.8584, lng:-70.9300},
-      	content:'<p>holi</p>'
-      },
-      {
-      	coords:{lat:42.7762, lng:-71.0773}
-      }
-      ];
-
-      addMarker({
-      	coords:{lat:42.4668, lng:-70.9495},
-      	iconImage:'http://1.bp.blogspot.com/-PxgBgllxJrE/VpgleojkmyI/AAAAAAAAEAs/NOibbG2gPUc/s1600/google-marker-preview1-177x300.png'});
-      
-      addMarker({coords:{lat:42.8584, lng:-70.9300}});
-      addMarker({coords:{lat:42.7762, lng:-71.0773}});
-
-
-      function addMarker(props){
-      	var marker = new google.maps.Marker({
-      		position:props.coords,
-            map: map,
-      	});
-
-      	if (props.iconImage){
-      		marker.setICon(props.iconImage);
-      	}
-
-      	if (props.content){
-      		var infoWindow = new google.maps.InfoWindow({
-      			content:props.content
-      		});
-      	}
-      }*/
+    })
+  })
+});

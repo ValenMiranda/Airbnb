162db694027f66939d1043179df436fca5c91c38
“Marion” <marionacp@gmail.com>
2017-08-09 08:41:44 -0400

    boton funcionando filtros con togle

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+
+//INICIO SECCION FILTRO DOS//
+$(document).ready(function(){ 
+   $('#mostrar').on('click',function(){
+      $('.filtrodos').toggle(500);
+   });
+});
+
+
+
+//FIN SECCION FILTRO DOS//
--- dist/js/script.js
+++ dist/js/script.js
@@ -20253,3 +20253,14 @@ if (jQuery) {
   };
 })(jQuery);
 
+
+//INICIO SECCION FILTRO DOS//
+$(document).ready(function(){ 
+   $('#mostrar').on('click',function(){
+      $('.filtrodos').toggle(500);
+   });
+});
+
+
+
+//FIN SECCION FILTRO DOS//
